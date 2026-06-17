641d37a9287d84863e7c6af905694d4576cb3a01
ruchi5286 <ruchikumari5286@gmail.com>
2026-06-17 12:34:11 +0530

    Updated Nextdot website changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Hero } from "../components/Hero";
+import { PartnersShowcase } from "../components/PartnersShowcase";
 import { SuccessStories } from "../components/SuccessStories";
 import { CapabilitiesGrid } from "../components/CapabilitiesGrid";
 import { Platforms } from "../components/Platforms";
@@ -13,6 +14,7 @@ export default function Home() {
       <SEO title="Domain Engineered AI Systems" description="Nextdot builds domain-engineered AI systems and enterprise-grade AI products for large enterprises." path="/" />
       <main>
       <Hero />
+      <PartnersShowcase />
       <CapabilitiesGrid />
       <SuccessStories />
       <Platforms />
